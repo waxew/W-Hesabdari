822395c7c1a86f72df5a6bcc9ab8101d50b92c55
waxew <4622.ar@gmail.com>
2026-09-07 16:17:24 +0330

    تکمیل Business Type های فروشگاهی در هسته Business Profile

--- app/src/main/java/com/hesabdari/business_profile/AccountingBusinessType.kt
+++ app/src/main/java/com/hesabdari/business_profile/AccountingBusinessType.kt
@@ -1,16 +1,35 @@
 /*
+================================================
 نام فایل:
 AccountingBusinessType.kt
 
 وظیفه:
-تعریف انواع کسب و کارهایی که می توانند از Core استفاده کنند.
+تعریف انواع کسب و کارهایی که می توانند از Core حسابداری استفاده کنند.
+
+ارتباط:
+BusinessProfile
+BusinessRegistry
+
+نکات توسعه:
+این فایل فقط طبقه بندی کسب و کار است.
+هیچ منطق اختصاصی کسب و کار نباید در اینجا قرار گیرد.
+================================================
 */
 
 package com.hesabdari.business_profile
 
 
 enum class AccountingBusinessType {
-    RETAIL,
+    MOBILE_STORE,
+    BOUTIQUE,
+    COSMETICS_STORE,
+    HOME_APPLIANCE_STORE,
+    AUTO_PARTS_STORE,
+    JEWELRY_STORE,
+    BOOK_STORE,
+    GROCERY_STORE,
+    PET_STORE,
+    OMNICHANNEL_STORE,
     MANUFACTURING,
     SERVICE_COMPANY,
     RESTAURANT,
