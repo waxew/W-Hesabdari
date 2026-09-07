9861e8b058d18dd49f355126e6da56f8dc2a0e3e
waxew <4622.ar@gmail.com>
2026-09-07 16:18:37 +0330

    افزودن Business Profile طلا و جواهر

---
+++ business_profile/profiles/jewelry_store_001.kt
@@ -0,0 +1,14 @@
+/*
+================================================
+نام فایل:
+jewelry_store_001.kt
+
+وظیفه:
+تعریف تنظیمات حسابداری طلا و جواهر.
+
+================================================
+*/
+
+package com.hesabdari.business_profile.profiles
+
+val JewelryStore001 = "jewelry_store_001"
