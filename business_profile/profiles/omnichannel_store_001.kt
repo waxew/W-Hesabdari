ae605fddc2a1268e5cc01674373f327298974162
waxew <4622.ar@gmail.com>
2026-09-07 16:18:56 +0330

    افزودن Business Profile فروشگاه چندکاناله

---
+++ business_profile/profiles/omnichannel_store_001.kt
@@ -0,0 +1,14 @@
+/*
+================================================
+نام فایل:
+omnichannel_store_001.kt
+
+وظیفه:
+تعریف تنظیمات حسابداری فروشگاه آنلاین و حضوری.
+
+================================================
+*/
+
+package com.hesabdari.business_profile.profiles
+
+val OmnichannelStore001 = "omnichannel_store_001"
