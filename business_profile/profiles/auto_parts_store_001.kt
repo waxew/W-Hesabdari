2a0935292bad88f92d4ab5b92d66f37abac9c7e7
waxew <4622.ar@gmail.com>
2026-09-07 16:18:33 +0330

    افزودن Business Profile قطعات خودرو

---
+++ business_profile/profiles/auto_parts_store_001.kt
@@ -0,0 +1,14 @@
+/*
+================================================
+نام فایل:
+auto_parts_store_001.kt
+
+وظیفه:
+تعریف تنظیمات حسابداری فروشگاه قطعات خودرو.
+
+================================================
+*/
+
+package com.hesabdari.business_profile.profiles
+
+val AutoPartsStore001 = "auto_parts_store_001"
