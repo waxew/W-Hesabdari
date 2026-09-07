1fb369475d37cb114ef65a78ef370398dfe6a898
waxew <4622.ar@gmail.com>
2026-09-07 15:34:15 +0330

    ایجاد انواع کسب و کار حسابداری

---
+++ app/src/main/java/com/hesabdari/business_profile/AccountingBusinessType.kt
@@ -0,0 +1,18 @@
+/*
+نام فایل:
+AccountingBusinessType.kt
+
+وظیفه:
+تعریف انواع کسب و کارهایی که می توانند از Core استفاده کنند.
+*/
+
+package com.hesabdari.business_profile
+
+
+enum class AccountingBusinessType {
+    RETAIL,
+    MANUFACTURING,
+    SERVICE_COMPANY,
+    RESTAURANT,
+    DISTRIBUTION
+}
