3daa0a223e2533bd93f7cce0700c4c872533cfe3
waxew <4622.ar@gmail.com>
2026-09-07 16:18:23 +0330

    افزودن Business Profile لوازم آرایشی

---
+++ business_profile/profiles/cosmetics_store_001.kt
@@ -0,0 +1,18 @@
+/*
+================================================
+نام فایل:
+cosmetics_store_001.kt
+
+وظیفه:
+تعریف تنظیمات نسخه حسابداری فروشگاه لوازم آرایشی.
+
+ارتباط:
+Business Profile System
+Module Configuration
+Attribute Schema
+================================================
+*/
+
+package com.hesabdari.business_profile.profiles
+
+val CosmeticsStore001 = "cosmetics_store_001"
