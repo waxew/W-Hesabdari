33fb43897074f525be9016b2c91d7cd3da832372
waxew <4622.ar@gmail.com>
2026-09-08 16:24:13 +0330

    Add repository dependency injection module foundation

---
+++ app/src/main/java/com/hesabdari/accounting_engine/di/RepositoryModule.kt
@@ -0,0 +1,11 @@
+package com.hesabdari.accounting_engine.di
+
+/**
+ * Dependency injection foundation for accounting repositories.
+ *
+ * This module will provide bindings between repository contracts
+ * and their implementations.
+ */
+object RepositoryModule {
+    // Repository bindings will be added here when DI framework is connected.
+}
