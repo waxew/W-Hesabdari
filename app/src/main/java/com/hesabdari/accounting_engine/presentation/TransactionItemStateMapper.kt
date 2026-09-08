85c4bf1468dcddacce7feefc4aa1c03ca2cdda13
waxew <4622.ar@gmail.com>
2026-09-08 18:13:35 +0330

    Fix transaction item state mapping types

--- app/src/main/java/com/hesabdari/accounting_engine/presentation/TransactionItemStateMapper.kt
+++ app/src/main/java/com/hesabdari/accounting_engine/presentation/TransactionItemStateMapper.kt
@@ -3,14 +3,11 @@ package com.hesabdari.accounting_engine.presentation
 import com.hesabdari.accounting_engine.domain.Transaction
 
 object TransactionItemStateMapper {
-
-    fun map(transaction: Transaction): TransactionItemState {
-        return TransactionItemState(
-            id = transaction.id,
-            title = transaction.referenceId,
-            amount = transaction.amount,
-            type = transaction.status.name,
-            date = null
-        )
-    }
+    fun map(transaction: Transaction): TransactionItemState = TransactionItemState(
+        id = transaction.id.toString(),
+        title = transaction.referenceId,
+        amount = transaction.amount,
+        type = transaction.status.name,
+        date = ""
+    )
 }
