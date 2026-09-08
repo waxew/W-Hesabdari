89b5a2122298a27289c78f1cd23f905dfa4dfb3a
waxew <4622.ar@gmail.com>
2026-09-08 17:59:25 +0330

    Fix ledger account ID mapping

--- app/src/main/java/com/hesabdari/accounting_engine/data/AccountingMapper.kt
+++ app/src/main/java/com/hesabdari/accounting_engine/data/AccountingMapper.kt
@@ -1,19 +1,13 @@
 package com.hesabdari.accounting_engine.data
 
 import com.hesabdari.accounting_engine.domain.LedgerAccount
-import com.hesabdari.accounting_engine.domain.Transaction
 
-/**
- * Converts accounting domain objects to persistence objects.
- */
+/** Converts accounting domain objects to persistence objects. */
 object AccountingMapper {
-
-    fun map(account: LedgerAccount): LedgerAccountEntity {
-        return LedgerAccountEntity(
-            id = account.id,
-            code = account.code,
-            name = account.name,
-            balance = account.balance
-        )
-    }
+    fun map(account: LedgerAccount): LedgerAccountEntity = LedgerAccountEntity(
+        id = account.id.toLongOrNull() ?: 0L,
+        code = account.code,
+        name = account.name,
+        balance = account.balance
+    )
 }
