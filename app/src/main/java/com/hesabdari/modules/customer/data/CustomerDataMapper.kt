c304675fd844fbf0391fa4db5ac919ac27c8042c
waxew <4622.ar@gmail.com>
2026-09-08 14:13:47 +0330

    Add Customer data mapper documentation layer

---
+++ app/src/main/java/com/hesabdari/modules/customer/data/CustomerDataMapper.kt
@@ -0,0 +1,21 @@
+/*
+================================================
+نام فایل:
+CustomerDataMapper.kt
+
+وظیفه:
+مدیریت تبدیل مدل های مختلف Customer.
+
+ارتباط:
+Domain Model <-> Database Model
+
+نکات توسعه:
+Entity های دیتابیس نباید مستقیما وارد Domain شوند.
+================================================
+*/
+
+package com.hesabdari.modules.customer.data
+
+object CustomerDataMapper {
+    // محل تبدیل Domain و Data Model در توسعه بعدی.
+}
